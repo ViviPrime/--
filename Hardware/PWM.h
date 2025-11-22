efa6246b16c231b514aa5c65db767d268efd6e93
VivPrime <2277921441@qq.com>
2025-11-22 19:46:20 +0800

    配置PWM

--- Hardware/PWM.h
+++ Hardware/PWM.h
@@ -2,6 +2,7 @@
 #define __PWM_H
 
 void PWM_Init(void);
-void PWM_SetCompare3(uint16_t Compare);
-void PWM_SetCompare4(uint16_t Compare);
+void PWM_SetCompare_Left(uint16_t Compare);//1号电机pwm设置函数
+void PWM_SetCompare_Right(uint16_t Compare);//2号电机pwm设置函数
+
 #endif
