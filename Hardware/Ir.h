754d8617e5559bc1156e707424913001edbc5e63
VivPrime <2277921441@qq.com>
2025-11-25 21:54:56 +0800

    Ir的修改上次未提交

--- Hardware/Ir.h
+++ Hardware/Ir.h
@@ -2,7 +2,14 @@
 #define __IR_H__
 
 void Ir_init(void);
-unsigned char Left_xunji(void);
-unsigned char Right_xunji(void);
+void IR_GetAllValues(unsigned char* sensor_values);
+unsigned char IR_GetSensor(unsigned char sensor_num);
+
+// 传感器编号定义
+#define IR_LEFTMOST    0   // 最左侧传感器
+#define IR_LEFT        1   // 左侧传感器  
+#define IR_MIDDLE      2   // 中间传感器
+#define IR_RIGHT       3   // 右侧传感器
+#define IR_RIGHTMOST   4   // 最右侧传感器
 
 #endif
